0e3d288e7d2aa0a6b90e6dbe90f0f933ec246b62
amare1990 <amaremek@gmail.com>
2023-02-21 00:09:13 +0300

    Migrate the migration file for the CategoryMyTransaction model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_191242) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_204830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_191242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "category_my_transactions", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "my_transaction_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_my_transactions_on_category_id"
+    t.index ["my_transaction_id"], name: "index_category_my_transactions_on_my_transaction_id"
   end
 
   create_table "my_transactions", force: :cascade do |t|
@@ -50,5 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_191242) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "category_my_transactions", "categories"
+  add_foreign_key "category_my_transactions", "my_transactions"
   add_foreign_key "my_transactions", "users"
 end
