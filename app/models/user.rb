94adee35f71b07fd010bf124e16f6c3c98c3e7d7
amare1990 <amaremek@gmail.com>
2023-02-20 23:39:36 +0300

    Add associations betwen User and cCategory and MyTransaction models, add name validations for User name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :my_transactions
+  has_many :categories
+
+  validates :name, presence: true, length: { in: 2..50 }
 end
