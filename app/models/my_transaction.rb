95ab685279953297c7eb377110af09a27be42f80
amare1990 <amaremek@gmail.com>
2023-02-21 00:30:20 +0300

    Add validations to MyTransaction model

--- app/models/my_transaction.rb
+++ app/models/my_transaction.rb
@@ -3,4 +3,7 @@ class MyTransaction < ApplicationRecord
 
   has_many :category_my_transactions
   has_many :categories, through: :category_my_transactions
+
+  validates :name, presence: true, length: {in: 1-30 }
+  validates :amount, presence: true, numerically: { only_float: true, greater_than: 0 }
 end
