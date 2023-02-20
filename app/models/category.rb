6c1712585f105fc9d0b95c652a98e193d27aa891
amare1990 <amaremek@gmail.com>
2023-02-21 00:04:45 +0300

    Add name validations and set associations between the categ model and the rest of the associated models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
+
+  has_many :category_my_transactions
+  has_many :my_transactions, through: :category_my_transactions
+
+  validates :name, presence: true, length:{in: 1..30 }
 end
