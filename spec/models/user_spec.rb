72d638bcbc5d552cc4b382c1e6d780637a2ce9e1
amare1990 <amaremek@gmail.com>
2023-02-21 11:36:13 +0300

    Add unit test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user = User.create!(name: "Endeshaw N. Alemneh", email: "endesh@gmail.com",
+    password: "09876543")
+  end
+
+  describe "\nmodel testing" do
+
+    it "\nshould display invalid input for empty full name" do
+      expect(@user).to be_valid
+    end
+
+    it "\nThe user full name should not be blank" do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "\n The user email should not be blank" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+  end
+
+  describe "\n model validation testing" do
+    subject do
+      @user2 = User.new(name: "Sh", email: "shibe@budgetapp.com", password: "09876543")
+    end
+
+    it "\n The user object should be invalid if the name char length is less than 5 characters" do
+      expect(subject).not_to be_valid
+    end
+  end
 end
