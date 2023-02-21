3b315bb78e1c981103d405638d9e03a47ed5ce5b
amare1990 <amaremek@gmail.com>
2023-02-21 12:51:02 +0300

    Implement unit tests for MyTransaction model

--- app/models/my_transaction.rb
+++ app/models/my_transaction.rb
@@ -4,6 +4,6 @@ class MyTransaction < ApplicationRecord
   has_many :category_my_transactions
   has_many :categories, through: :category_my_transactions
 
-  validates :name, presence: true, length: {in: 1-30 }
-  validates :amount, presence: true, numerically: { only_float: true, greater_than: 0 }
+  validates :name, presence: true, length: {in: 3..30 }
+  validates :amount, presence: true, numericality: { only_float: true, greater_than: 0 }
 end
