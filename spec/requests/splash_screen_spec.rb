f0f503682cfce3794a28bcf8f1f06405e9b29a72
amare1990 <amaremek@gmail.com>
2023-02-22 15:50:57 +0300

    Add route path testing for SplashScreen controller

--- spec/requests/splash_screen_spec.rb
+++ spec/requests/splash_screen_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "SplashScreens", type: :request do
+  before(:each) { get root_path }
   describe "GET /show" do
-    it "returns http success" do
-      get "/splash_screen/show"
+    it "should return http success" do
+      get "/"
       expect(response).to have_http_status(:success)
     end
+
+    it "should render the correct template, i.e., 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
   end
 
 end
